04f3722c4a4ef8c70d065f58ed7e7e304e1c6b54
Vladislav Kovalenko <valdkov135@gmail.com>
2025-11-03 13:11:37 +0300

    Initialize project and implement basic bot logic
    -Add project structure
    -Configur .gitignore
    -Add command handlers: /start /help and unknowen

---
+++ cmd/app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+	tgbot "mispilkabot/internal/telegram"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	mispilkaBot := tgbot.NewBot(bot)
+	mispilkaBot.Start()
+}
---
+++ internal/telegram/bot.go
@@ -0,0 +1,56 @@
+package bot
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Bot struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewBot(bot *tgbotapi.BotAPI) *Bot {
+	return &Bot{bot: bot}
+}
+
+func (b *Bot) Start() {
+	log.Printf("Authorized on account %s", b.bot.Self.UserName)
+
+	b.handleUpdates(b.initUpdatesChanel())
+}
+
+func (b *Bot) initUpdatesChanel() tgbotapi.UpdatesChannel {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	return b.bot.GetUpdatesChan(u)
+}
+
+func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.IsCommand() {
+			b.handleCommand(update.Message)
+		}
+	}
+}
+
+func (b *Bot) handleCommand(message *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "")
+
+	switch message.Command() {
+	case "start":
+		msg.Text = "start command"
+	case "help":
+		msg.Text = "help command"
+	default:
+		msg.Text = "I don't know that command"
+	}
+
+	if _, err := b.bot.Send(msg); err != nil {
+		log.Panic(err)
+	}
+}
