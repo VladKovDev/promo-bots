36ae3fac006a8f6287474fa53079863d71f821f2
DanilaBesk <danilbeskorovaev546@gmail.com>
2025-12-18 23:43:28 +0300

    feat: initialize migrations

--- makefile
+++ makefile
@@ -1,7 +1,15 @@
-.PHONY: build run
+.PHONY: migrate-up migrate-down migrate-reset migrate-status migrate-create
 
 BINARY_NAME=promo-bots
 BUILD_DIR=./bin
+MIGRATIONS_DIR := ./migrations
+
+
+# Colors for output
+COLOR_RESET=\033[0m
+COLOR_BOLD=\033[1m
+COLOR_GREEN=\033[32m
+COLOR_YELLOW=\033[33m
 
 export CONFIG_PATH ?= ./configs
 
@@ -13,3 +21,52 @@ build:
 run: build
 	@echo "Running $(BINARY_NAME)"
 	@$(BUILD_DIR)/$(BINARY_NAME)
+	
+
+migrate-up: ## Применить все миграции
+	@echo "$(COLOR_YELLOW)Running migrations...$(COLOR_RESET)"
+	@if [ -z "$(DATABASE_URL)" ]; then \
+		echo "ERROR: DATABASE_URL is not set"; \
+		exit 1; \
+	fi
+	@GOOSE_DRIVER=postgres GOOSE_DBSTRING=$(DATABASE_URL) goose -dir $(MIGRATIONS_DIR) up
+	@echo "$(COLOR_GREEN)Migrations applied!$(COLOR_RESET)"
+
+migrate-down: ## Откатить последнюю миграцию
+	@echo "$(COLOR_YELLOW)Rolling back last migration...$(COLOR_RESET)"
+	@if [ -z "$(DATABASE_URL)" ]; then \
+		echo "ERROR: DATABASE_URL is not set"; \
+		exit 1; \
+	fi
+	@GOOSE_DRIVER=postgres GOOSE_DBSTRING=$(DATABASE_URL) goose -dir $(MIGRATIONS_DIR) down
+	@echo "$(COLOR_GREEN)Migration rolled back!$(COLOR_RESET)"
+
+migrate-reset: ## Сбросить все миграции
+	@echo "$(COLOR_YELLOW)Resetting all migrations...$(COLOR_RESET)"
+	@if [ -z "$(DATABASE_URL)" ]; then \
+		echo "$(COLOR_YELLOW)ERROR: DATABASE_URL is not set$(COLOR_RESET)"; \
+		exit 1; \
+	fi
+	@GOOSE_DRIVER=postgres GOOSE_DBSTRING=$(DATABASE_URL) goose -dir $(MIGRATIONS_DIR) reset
+	@echo "$(COLOR_GREEN)All migrations reset!$(COLOR_RESET)"
+
+migrate-status: ## Показать статус миграций
+	@if [ -z "$(DATABASE_URL)" ]; then \
+		echo "ERROR: DATABASE_URL is not set"; \
+		exit 1; \
+	fi
+	@GOOSE_DRIVER=postgres GOOSE_DBSTRING=$(DATABASE_URL) goose -dir $(MIGRATIONS_DIR) status
+
+migrate-create: ## Создать новую миграцию (use: make migrate-create NAME=migration_name)
+	@if [ -z "$(name)" ]; then \
+		echo -e "$(COLOR_YELLOW)ERROR: migration name is required$(COLOR_RESET)"; \
+		exit 1; \
+	fi
+	@echo -e "$(COLOR_YELLOW)Creating new migration: $(NAME)...$(COLOR_RESET)"
+	@goose -dir $(MIGRATIONS_DIR) create $(name) sql
+	@echo -e "$(COLOR_GREEN)Migration created!$(COLOR_RESET)
+
+sqlc-gen: ## Generate code from SQL
+	@echo "$(COLOR_YELLOW)Generating code from SQL...$(COLOR_RESET)"
+	sqlc generate
+	@echo "$(COLOR_GREEN)Code generated!$(COLOR_RESET)"
---
+++ migrations/20251218185910_init_tables.sql
@@ -0,0 +1,153 @@
+-- +goose Up
+
+-- Таблица пользователей
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    telegram_id BIGINT,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    "role" TEXT NOT NULL CHECK (role IN ('admin', 'user')),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    blocked_at TIMESTAMP
+);
+
+-- Таблица телеграм-ботов
+CREATE TABLE telegram_bots (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    owner_id UUID NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
+    bot_id BIGINT,
+    username TEXT NOT NULL,
+    first_name TEXT,
+    encryped_token TEXT NOT NULL, 
+    "status" TEXT NOT NULL,
+    last_error TEXT,
+    last_checked_at TIMESTAMP,
+    revoked_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- Таблица скриптов
+CREATE TABLE scripts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" TEXT UNIQUE NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    private_group_id UUID
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP,
+);
+
+-- Таблица сообщений
+CREATE TABLE messages (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    content TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP,
+    no_script BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- Таблица шагов скриптов
+CREATE TABLE script_steps (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    script_id UUID NOT NULL REFERENCES scripts(id) ON DELETE CASCADE,
+    message_id UUID NOT NULL REFERENCES messages(id) ON DELETE RESTRICT,
+    "order" INT NOT NULL,
+    channel TEXT NOT NULL,
+    timing INT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP,
+    skip_on_error BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+
+-- Таблица прогресса пользователя по скриптам
+CREATE TABLE script_progress (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    script_id UUID REFERENCES scripts(id) ON DELETE SET NULL,
+    current_step_id UUID REFERENCES script_steps(id) ON DELETE SET NULL,
+    "status" TEXT NOT NULL,
+    step_started_at TIMESTAMP,
+    started_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    finished_at TIMESTAMP
+);
+
+-- Таблица доставки сообщений шагов
+CREATE TABLE script_progress_delivery (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    message_id UUID REFERENCES messages(id) ON DELETE SET NULL,
+    step_id UUID REFERENCES script_steps(id) ON DELETE SET NULL,
+    script_progress_id UUID REFERENCES script_progress(id) ON DELETE RESTRICT,
+    sent_at TIMESTAMP,
+    channel TEXT NOT NULL,
+    "snapshot" JSONB NOT NULL,
+    telegram_message_id TEXT NOT NULL
+);
+
+-- Таблица медиа файлов сообщений
+CREATE TABLE message_media (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    uploaded_by UUID REFERENCES users(id) ON DELETE SET NULL,
+    message_id UUID REFERENCES messages(id) ON DELETE SET NULL,
+    storage_key TEXT NOT NULL,
+    ext TEXT NOT NULL,
+    "size" BIGINT NOT NULL,
+    mime_type TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP
+);
+
+-- Таблица кнопок сообщений
+CREATE TABLE message_buttons (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    message_id UUID NOT NULL REFERENCES messages(id) ON DELETE CASCADE,
+    "text" TEXT NOT NULL,
+    "url" TEXT, -- также сделать команду /command 
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP
+);
+
+-- Таблица планирования шагов
+CREATE TABLE scheduled_steps (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    script_progress_id UUID NOT NULL REFERENCES script_progress(id) ON DELETE CASCADE,
+    execute_at TIMESTAMP NOT NULL,
+    "status" TEXT NOT NULL,
+    send_at TIMESTAMP,
+    sent_at TIMESTAMP,
+    attempts INT NOT NULL DEFAULT 0,
+    last_error TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- Таблица истории изменений
+CREATE TABLE history (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    entity_id UUID NOT NULL,
+    entity_table TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    meta JSONB,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+-- Удаление таблиц
+DROP TABLE IF EXISTS history;
+DROP TABLE IF EXISTS scheduled_steps;
+DROP TABLE IF EXISTS message_buttons;
+DROP TABLE IF EXISTS message_media;
+DROP TABLE IF EXISTS script_progress_delivery;
+DROP TABLE IF EXISTS script_progress;
+DROP TABLE IF EXISTS script_steps;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS scripts;
+DROP TABLE IF EXISTS telegram_bots;
+DROP TABLE IF EXISTS users;
